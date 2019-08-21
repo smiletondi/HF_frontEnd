f136ac1ac9e1ca176b5f31d8296a39fe337c6381
smiletondi <smiletondi@gmail.com>
2019-08-21 23:46:47 +0100

    Updates

---
+++ precache-manifest.eafca5c9f92d610ec092b43b2b993ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f69f71dd6ecd1e2f84cf6e673af55c",
+    "url": "smiletondi.github.io/HF_frontEnd/index.html"
+  },
+  {
+    "revision": "17dad2755a8554d11f4c",
+    "url": "smiletondi.github.io/HF_frontEnd/static/css/main.f6b7274d.chunk.css"
+  },
+  {
+    "revision": "37b0fc38e9eaabdb263c",
+    "url": "smiletondi.github.io/HF_frontEnd/static/js/2.c8da893f.chunk.js"
+  },
+  {
+    "revision": "17dad2755a8554d11f4c",
+    "url": "smiletondi.github.io/HF_frontEnd/static/js/main.ed359d3b.chunk.js"
+  },
+  {
+    "revision": "b654e7e7af806ebca113",
+    "url": "smiletondi.github.io/HF_frontEnd/static/js/runtime~main.e815d38a.js"
+  },
+  {
+    "revision": "5e02af51a044f8d6de136e2c7e8d6c9a",
+    "url": "smiletondi.github.io/HF_frontEnd/static/media/spinner1.5e02af51.gif"
+  }
+]);
